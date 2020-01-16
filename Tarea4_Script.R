f85bc0bdb6e2eca9fd8a64a61499a967904fa280
Ricardo Garbanzo <rjgarbanzo@gmail.com>
2020-01-15 21:46:29 -0600

    Finalizado Ejercicio 1

--- Tarea4_Script.R
+++ Tarea4_Script.R
@@ -1,6 +1,6 @@
 #Eje 1
-#A
 
+# Punto A
 # Variando k desde 1 hasta 20, para estos datos determine usando el Codo de Jambu el
 # mejor valor para k en el m´etodo k−medias. Utilice nstart=5 y iter.max = 2000, ¿que
 # significan estos parametros? ¿Por qu´e es importante usar estos valores?
@@ -39,4 +39,67 @@ legend("topright",legend = c("Hartigan","Lloyd","Forgy","MacQueen"),
 # Es importante asignar un nstart bajo ya que de lo contrario se obtienen clusters vacios 
 
 
+# Punto B
 
+# Usando k = 3, nstart=5 y iter.max = 2000 en el metodo k−medias determine cual
+# de los algoritmos “Hartigan-Wong”, “Lloyd”, “Forgy” y “MacQueen”funciona mejor para
+# estos datos en el sentido de que minimizan la inercia intra–clases.
+
+
+#Declaracion
+Hartigan<-0
+Lloyd<-0
+Forgy<-0
+MacQueen<-0
+# Re calculo de inercias
+for(i in 1:50) {
+  grupos<-kmeans(datos,3,iter.max=2000,nstart=5,algorithm = "Hartigan-Wong")
+  Hartigan<-Hartigan+grupos$tot.withinss
+  grupos<-kmeans(datos,3,iter.max=2000,nstart=5,algorithm = "Lloyd")
+  Lloyd<-Lloyd+grupos$tot.withinss
+  grupos<-kmeans(datos,3,iter.max=2000,nstart=5,algorithm = "Forgy")
+  Forgy<-Forgy+grupos$tot.withinss
+  grupos<-kmeans(datos,3,iter.max=2000,nstart=5,algorithm = "MacQueen")
+  MacQueen<-MacQueen+grupos$tot.withinss
+}  
+Hartigan/50
+Lloyd/50
+Forgy/50
+MacQueen/50
+
+
+# MacQueen presentan la menor inercia intra clases.
+
+
+
+
+
+# Punto C
+
+# Repita el ejercicio anterior usando k = 3, nstart=1 y iter.max = 20. ¿Que se observa?
+# ¿A que se debe la diferencia? ¿Cual resultado es mas creıble el del punto b) o el del punto c)?
+
+#Declaracion
+Hartigan<-0
+Lloyd<-0
+Forgy<-0
+MacQueen<-0
+# Re calculo de inercias
+for(i in 1:50) {
+  grupos<-kmeans(datos,3,iter.max=20,nstart=1,algorithm = "Hartigan-Wong")
+  Hartigan<-Hartigan+grupos$tot.withinss
+  grupos<-kmeans(datos,3,iter.max=20,nstart=1,algorithm = "Lloyd")
+  Lloyd<-Lloyd+grupos$tot.withinss
+  grupos<-kmeans(datos,3,iter.max=20,nstart=1,algorithm = "Forgy")
+  Forgy<-Forgy+grupos$tot.withinss
+  grupos<-kmeans(datos,3,iter.max=20,nstart=1,algorithm = "MacQueen")
+  MacQueen<-MacQueen+grupos$tot.withinss
+}  
+Hartigan/50
+Lloyd/50
+Forgy/50
+MacQueen/50
+
+
+# Se da una disminucion de la inercia intra clase al existir mas componentes dentro de cada cluster
+# Tomando en cuenta que apartir de K=4 se estabiliza el tot.withinss sería mas confiable el punto B
